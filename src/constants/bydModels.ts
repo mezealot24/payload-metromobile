abcfa9ee2c10d5fc2f586469df24de8a139116a4
Mezealot24 <67420846+mezealot24@users.noreply.github.com>
2026-07-11 04:53:15 +0700

    feat(promotions): add July campaign model slugs

--- src/constants/bydModels.ts
+++ src/constants/bydModels.ts
@@ -1,4 +1,8 @@
 export const BYD_MODEL_OPTIONS = [
+  { label: 'BYD SEALION 5 DM-i', value: 'sealion-5-dmi' },
+  { label: 'BYD SEAL 6', value: 'seal-6' },
+  { label: 'BYD ATTO 2', value: 'atto-2' },
+  { label: 'BYD ATTO 1', value: 'atto-1' },
   { label: 'BYD SEAL 5 DM-i', value: 'seal-5-dmi' },
   { label: 'BYD SEALION 7', value: 'sealion7' },
   { label: 'BYD M6', value: 'm6' },
@@ -7,4 +11,3 @@ export const BYD_MODEL_OPTIONS = [
   { label: 'NEW BYD DOLPHIN', value: 'dolphin' },
   { label: 'BYD SEAL', value: 'seal' },
 ] as const
-
